472a005af8e8c47d6c8201167b4d8cefd973be39
Ciro Simonetti <ciro.simonetti207@gmail.com>
2015-05-09 12:21:43 +0200

    stò committando. . .

--- main.c
+++ main.c
@@ -19,6 +19,25 @@ struct s_studente {
 typedef struct s_studente studente;
 
 int main(int argc, char** argv) {
+    FILE *puntafile;
+    studente elenco;
+    int i;
+    puntafile=fopen(FNAME, "w");
+    
+            fprintf(puntafile, "Nome;Cognome;Eta';Classe\n");
+        for (i=0;i<N;i++) {
+            printf("Studente %d:\n", i+1);
+            printf("Nome: ");
+            scanf("%s", elenco.nome);
+            printf("Cognome: ");
+            scanf("%s", elenco.cognome);
+            printf("Eta': ");
+            scanf("%d", &elenco.eta);
+            printf("Classe: ");
+            scanf("%s", elenco.classe);
+                fprintf(puntafile, "%s;%s;%d;%s\n", elenco.nome, elenco.cognome, elenco.eta, elenco.classe);
+        }
+        fclose(puntafile);
     
     return (EXIT_SUCCESS);
 }
--- nbproject/Makefile-impl.mk
+++ nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=Ese036-ElencoStudentiSalvaCSV
+PROJECTNAME=SalvaCSV
 
 # Active Configuration
 DEFAULTCONF=Debug
